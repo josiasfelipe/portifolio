6484a6f8ef919eacb9b868216e44e1a5cbc6b61d
Josias Felipe <56060680+josiasfelipe@users.noreply.github.com>
2023-01-23 14:21:28 -0300

    uWu

--- script/script.js
+++
@@ -1,7 +0,0 @@
-var tela = document.getElementById('#body').clientWidth;
-var img = document.getElementById('foto');
-function changeImg(){
-    if ( tela >= '700' ){
-        img.scr = "images/Opa.jpg"
-    }
-}
